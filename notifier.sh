be76ef4e1149fa5bd9b655796b8be80ba222034e
eviangel <friends.mh@hotmail.com>
2024-02-26 19:19:42 +0100

    fixed a typo

--- notifier.sh
+++ notifier.sh
@@ -10,10 +10,10 @@ echo "Checking for container: $CONTAINER_NAME"
 echo "Containers found: $CONTAINER_RUNNING"
 
 # If the container is not running, then send the message
-if [ ! -z "$CONTAINER_RUNNING" ]; then
+if [ -z "$CONTAINER_RUNNING" ]; then
     # Use Python to read the Webhook URL from the JSON file
     WEBHOOK_URL=$(python3 -c "import json; print(json.load(open('notifier_config.json'))['webhook_url'])")
-    MESSAGE="The Docker container for koii node is not running! please check your VPS and do: docker compose up."
+    MESSAGE="Your koii node Docker container isn't running! Please check your VPS. To start it, go to the VPS-task directory and run: docker compose up."
 
     # Check if the WEBHOOK_URL is empty
     if [ -z "$WEBHOOK_URL" ]; then
